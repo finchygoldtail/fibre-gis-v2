b538f7025261dd03c56756d9bde478431b8f43b6
Alistair Grantham <Alistairlgrantham@gmail.com>
2026-04-26 16:39:25 +0100

    Update Firebase config for Vercel

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt-M8Tn0YKQPu6IMfAkCO8wu_OTNNXmYg",
-  authDomain: "fibre-gis.firebaseapp.com",
-  projectId: "fibre-gis",
-  storageBucket: "fibre-gis.firebasestorage.app",
-  messagingSenderId: "156986555833",
-  appId: "1:156986555833:web:ea0efbd97bfd26d6570a68",
+  apiKey: "AIzaSyBjihPWC3aV5ae0DOSkk_34xHvmjeAtikA",
+  authDomain: "fibre-gis-v2.firebaseapp.com",
+  projectId: "fibre-gis-v2",
+  storageBucket: "fibre-gis-v2.firebasestorage.app",
+  messagingSenderId: "886932988702",
+  appId: "1:886932988702:web:c9bbe13e9e75629cdf5f0f"
 };
 
 const app = initializeApp(firebaseConfig);
